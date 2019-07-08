8dfef1b97c073cc6643533b2a44212b244c43814
Sadman <sadmanh23@gmail.com>
2019-07-08 21:08:36 +0600

    Add protected meta to dashboard view

--- src/router.js
+++ src/router.js
@@ -23,7 +23,10 @@ export default new Router({
 		{
 			path: '/',
 			name: 'dashboard',
-			component: Dashboard
+			component: Dashboard,
+			meta: {
+				protected: true
+			}
 		},
 		{
 			path: '/about',
