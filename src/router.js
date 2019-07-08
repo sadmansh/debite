a37781ab45aaac1edf0c1f39588787b40141c5ca
Sadman <sadmanh23@gmail.com>
2019-07-09 01:33:50 +0600

    Change dashboard route and add home route

--- src/router.js
+++ src/router.js
@@ -22,6 +22,11 @@ export default new Router({
 		},
 		{
 			path: '/',
+			name: 'home',
+			component: Dashboard,
+		},
+		{
+			path: '/dashboard',
 			name: 'dashboard',
 			component: Dashboard,
 			meta: {
@@ -35,9 +40,6 @@ export default new Router({
 			// this generates a separate chunk (about.[hash].js) for this route
 			// which is lazy-loaded when the route is visited.
 			component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-			meta: {
-				protected: true
-			}
 		}
 	]
 })
